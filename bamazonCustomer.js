f8ac10956ffcf1d39705ded299061f9a0a142739
M. Pennella <michael.pennella@gmail.com>
2018-10-30 20:47:29 -0700

    Added library requirements, plus comments to lay out general program flow

---
+++ bamazonCustomer.js
@@ -0,0 +1,14 @@
+const inquirer = require("inquirer")
+const mysql = require("mysql")
+
+// List all items for sale
+
+// Prompt user to enter ID of item to buy
+
+// Prompt user to enter quantity they wish to purchase
+
+// Check for sufficient quantity to purchase
+
+// If enough, update database with new stock level
+
+// Show customer cost of purchase
